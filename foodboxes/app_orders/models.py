41d2a07016a74bae3bd60135403097809912c3ee
Arif_Gafizov <agafizov@gmail.com>
2021-01-17 17:57:07 +0300

    Add Order model

--- foodboxes/app_orders/models.py
+++ foodboxes/app_orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from app_users.models import User
+from app_carts.models import Cart
+from app_orders.data import choices
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField()
+    delivery_at = models.DateTimeField()
+    recipient = models.ForeignKey(User, related_name='orders', on_delete=models.CASCADE)
+    address = models.CharField(max_length=256)
+    cart = models.ForeignKey(Cart, related_name='orders', on_delete=models.CASCADE)
+    status = models.CharField(max_length=100, choices=choices)
+    total_cost = models.DecimalField(max_digits=13, decimal_places=2)
