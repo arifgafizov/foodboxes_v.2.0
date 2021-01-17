39d8d4c2f885454419b5ce3632aad0629c0bd046
Arif_Gafizov <agafizov@gmail.com>
2021-01-17 17:56:36 +0300

    Add Cart and CartItem model

--- foodboxes/app_carts/models.py
+++ foodboxes/app_carts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from app_items.models import Item
+from app_users.models import User
+
+
+class Cart(models.Model):
+    items = models.ManyToManyField(Item, related_name='carts')
+    user = models.ForeignKey(User, related_name='carts', on_delete=models.CASCADE)
+
+class CartItem(models.Model):
+    item = models.ForeignKey(Item, related_name='cart_items', on_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, related_name='cart_items', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=13, decimal_places=2)
+
